a544d7536f018cf184f3e05e24a5a2e890d654d8
Vladislav Bochkarev <bochkarev@gnivc.ru>
2024-01-21 14:58:40 +0300

    Убрал ямл-фабрику из AppCongig

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,13 @@
 package org.example;
 
-import org.example.config.AppConfig;
+import org.example.config.DefaultAppConfig;
 import org.example.bean.manager.AppManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
   public static void main(String[] args) {
-    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    ApplicationContext context = new AnnotationConfigApplicationContext(DefaultAppConfig.class);
     AppManager appManager = context.getBean(AppManager.class);
     appManager.doWork();
   }
