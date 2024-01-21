8e2626efb8bce657161127ac6aa5b6a254cb02f0
Vladislav Bochkarev <bochkarev@gnivc.ru>
2024-01-21 15:03:54 +0300

    Убрал Profile(default) из DefaultAppConfig

--- src/main/java/org/example/config/DefaultAppConfig.java
+++ src/main/java/org/example/config/DefaultAppConfig.java
@@ -2,12 +2,10 @@ package org.example.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @ComponentScan("org.example.bean")
 @Configuration
 @PropertySource("classpath:contacts.properties")
-@Profile("default")
 public class DefaultAppConfig {
 }
